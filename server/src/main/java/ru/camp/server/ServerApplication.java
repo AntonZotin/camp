8334ee37ed1b22d00520c0fadb432f409feafa61
Антон Зотин <toxin88@mail.ru>
2025-07-19 20:30:44 +0300

    init

--- server/src/main/java/ru/camp/server/ServerApplication.java
+++ server/src/main/java/ru/camp/server/ServerApplication.java
@@ -2,40 +2,11 @@ package ru.camp.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Autowired;
-import ru.camp.server.model.User;
-import ru.camp.server.model.Role;
-import ru.camp.server.model.UserType;
-import ru.camp.server.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ServerApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner createAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			if (userRepository.findByUsername("admin").isEmpty()) {
-				User admin = new User();
-				admin.setUsername("admin");
-				admin.setEmail("admin@example.org");
-				admin.setPassword(passwordEncoder.encode("admin"));
-				admin.setRole(Role.ADMIN);
-				admin.setUserType(UserType.ADMIN);
-				userRepository.save(admin);
-				logger.info("Default admin user created: login=admin, password=admin");
-			}
-		};
 	}
 }
