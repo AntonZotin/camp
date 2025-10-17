58e509eaa0a0d2612e739b121982eb0ef5c39724
Антон Зотин <toxin88@mail.ru>
2025-10-17 22:36:17 +0300

    init

--- server/src/main/java/ru/camp/server/controller/AdminExportController.java
+++ server/src/main/java/ru/camp/server/controller/AdminExportController.java
@@ -4,7 +4,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
